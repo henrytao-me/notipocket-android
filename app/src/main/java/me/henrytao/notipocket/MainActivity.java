799e7cae34ccf3c74cee89b4eeb61d3c3615bdb0
Henry Tao <hi@henrytao.me>
2014-09-21 16:02:57 +0700

    added dimension into o xml

--- app/src/main/java/me/henrytao/notipocket/MainActivity.java
+++ app/src/main/java/me/henrytao/notipocket/MainActivity.java
@@ -3,6 +3,7 @@ package me.henrytao.notipocket;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
